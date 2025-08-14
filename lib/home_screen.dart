5f44da23b26d43db4e6663191c608c961a8471dd
nirmal21332 <nirmal93514@gmail.com>
2025-08-14 10:30:40 +0530

    "Initial"

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx/counter_controller.dart';
+import 'package:getx/slider_controller.dart';
 import 'package:getx/screentwo.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,25 +11,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final CounterController controller = Get.put(CounterController());
+  final SliderController controller = Get.put(SliderController());
+  double opacity = 0.5;
 
   @override
   Widget build(BuildContext context) {
-    print('Hello World');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
         centerTitle: true,
       ),
-      body: Center(
-        child:Obx((){
-          return Text(controller.count.toString(),style: TextStyle(fontSize: 50),);
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          controller.Increment();
-        },
+      body: Column(
+        children: [
+          Obx(() {
+            return Container(
+              height: 200,
+              width: 200,
+              color: Colors.red.withOpacity(controller.opacity.value),
+            );
+          }),
+          Obx(() {
+            return Slider(
+                value: controller.opacity.value,
+                onChanged: (val) {
+                  controller.setOpacity(val);
+                });
+          }),
+        ],
       ),
     );
   }
